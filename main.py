b9545ebd9b4296d0ef495cd8dcbc87718e6ea58e
Shaiara Noshin <shaiara.noshin@g.bracu.ac.bd>
2025-09-02 14:13:07 +0600

    Create house and implement palette system

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 
-def display():
+PALETTE = {
+    "tin" : (0.659, 0.635, 0.616),
+    "skyblue" : (0.725, 0.898, 0.91),
+    "brown" : (0.69, 0.533, 0.337)
+}
+
+def getColor(k):
+    return PALETTE.get(k, (1, 1, 1))
+
+def drawHouse():
+    rx, ry = 0, 0
+    rh, rw = 140, 260
+    bh, bw = 1.5*rh, 0.75*rw
+    bx, by = rx, ry - (rh/2 + bh/2)
+    
+    # Roof
     glBegin(GL_TRIANGLES)
-    glColor3f(0, 0, 1)
-    glVertex2f(-30, 0)
-    glVertex2f(30, 0)
-    glVertex2f(0, 45)
+    glColor3f(*getColor("tin"))
+    glVertex2f(rx, ry + rh/2)
+    glVertex2f(rx - rw/2, ry - rh/2)
+    glVertex2f(rx + rw/2, ry - rh/2)
     glEnd()
+
+    # Body
+    glBegin(GL_QUADS)
+    glColor3f(*getColor("brown"))
+    glVertex2f(bx + bw/2, by + bh/2)
+    glVertex2f(bx - bw/2, by + bh/2)
+    glVertex2f(bx - bw/2, by - bh/2)
+    glVertex2f(bx + bw/2, by - bh/2)
+    glEnd()
+
+def display():
+    glClear(GL_COLOR_BUFFER_BIT)
+    drawHouse()
     glutSwapBuffers()
 
 def animate():
@@ -15,13 +43,14 @@ def animate():
     
 def main():
     glutInit()
-    glutInitDisplayMode(GLUT_RGBA)
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE)
     glutInitWindowPosition(0, 0)
-    glutInitWindowSize(400, 800)
+    glutInitWindowSize(400, 600)
     glutCreateWindow(b"A rainy world")
     glutDisplayFunc(display)
     glutIdleFunc(animate)
     glMatrixMode(GL_PROJECTION)
     glOrtho(-200, 200, -400, 400, -1, 1)
+    glClearColor(*getColor("skyblue"), 1)
     glutMainLoop()
 if __name__ == '__main__': main()
